332acca506959f84ef9320b2d0506fee93eef7e8
Viktor <karasssuper@yandex.ru>
2020-11-12 01:00:38 +0300

    4 try

--- src/main.c
+++ src/main.c
@@ -2,15 +2,19 @@
 
 void other(){
     printf("Congratulations!\n");
+    fflush(stdout);
     printf("You have entered in the secret function!\n");
+    fflush(stdout);
 }
 
 void input(){
     char buffer[20];
 
     printf("Enter some text:\n");
+    fflush(stdout);
     scanf("%s", buffer);
     printf("You entered: %s\n", buffer);
+    fflush(stdout);
 }
 
 int main(){
